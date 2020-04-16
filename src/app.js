96dbb7536bfbbf429e2bf1c4a8af624e6d141461
Gerard Hynes <gkhynes@gmail.com>
2020-04-16 20:58:45 +0100

    Add Header component

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
+import Header from "./components/header";
 import "./scss/app.scss";
+
+const app = () => {
+  document.getElementById("header").innerHTML = Header();
+};
+
+// Initialize app
+app();
 
 console.log("Your JavaScript works!");
---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+  const template = `
+    <header>
+    <h1>Get started quickly with Parcel</h1>
+    <p>This is a boilerplate for a vanilla JavaScript workflow</p>
+    </header>
+  `;
+
+  return template;
+};
+
+export default Header;
